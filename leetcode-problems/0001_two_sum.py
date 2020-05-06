a773745a09903c108e2066545dd234f1a09b3bbe
Varad Bhogayata <vbhogayata@gmail.com>
2020-05-06 11:40:02 +0530

    add tags

--- leetcode-problems/0001_two_sum.py
+++ leetcode-problems/0001_two_sum.py
@@ -1,4 +1,6 @@
 """
+Tags: Array, Hash Table, FB, Microsoft, Amazon, Uber, LinkedIn, Apple, Airbnb, Yelp, Yahoo, Adobe, Dropbox
+
 Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
