420bd063ab22b8c49572db4cf45cd5435cd1015b
Varad Bhogayata <vbhogayata@gmail.com>
2020-05-18 08:29:24 +0530

    add daily challenge

--- leetcode-problems/0438_find_all_anagrams_in_a_string.py
+++ leetcode-problems/0438_find_all_anagrams_in_a_string.py
@@ -1,15 +1,18 @@
 import collections
 
 """
-
-My Approach:
+My Approach (also approach-1):
 - dictionary keeping the count of the subarray of len(p).
 - if the count of substring(s) == count of p, then append the index.
+- O(Ns + Np), O(1) (as max 26 characters)
+
+Approach 2: Sliding Window with Array
+- use array of size 26 to store frequency
 - O(Ns + Np), O(1) (as max 26 characters)
 """
 
 class Solution:
-    def findAnagrams(self, s: str, p: str) -> List[int]:
+    def findAnagrams1(self, s: str, p: str) -> List[int]:
         ns, np = len(s), len(p)
         if ns < np:
             return []
@@ -39,3 +42,29 @@ class Solution:
                 ans.append(idx - np + 1)
         return ans
 
+    def findAnagrams2(self, s: str, p: str) -> List[int]:
+        ns, np = len(s), len(p)
+        if ns < np:
+            return []
+
+        p_count, s_count = [0] * 26, [0] * 26
+        # build reference array using string p
+        for ch in p:
+            p_count[ord(ch) - ord('a')] += 1
+
+        output = []
+        # sliding window on the string s
+        for i in range(ns):
+            # add one more letter
+            # on the right side of the window
+            s_count[ord(s[i]) - ord('a')] += 1
+            # remove one letter
+            # from the left side of the window
+            if i >= np:
+                s_count[ord(s[i - np]) - ord('a')] -= 1
+            # compare array in the sliding window
+            # with the reference array
+            if p_count == s_count:
+                output.append(i - np + 1)
+
+        return output
